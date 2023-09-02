6b9ceef996a235b75b6ddc8761bee1942f01b8c2
nikoagge <nikoagge@gmail.com>
2023-09-02 20:29:37 +0300

    Initial Commit

---
+++ ToDoListInSwiftUI/ToDoListInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListInSwiftUIApp.swift
+//  ToDoListInSwiftUI
+//
+//  Created by Nikos Aggelidis on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
